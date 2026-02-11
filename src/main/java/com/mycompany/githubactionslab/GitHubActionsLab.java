5a853a7e76484593dd56ada5dc907a0fdc441599
Krishna Patel <pate6047@sheridancollege.ca>
2026-02-11 13:33:31 -0500

    Add initial second time setup changes

---
+++ src/main/java/com/mycompany/githubactionslab/GitHubActionsLab.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.githubactionslab;
+
+/**
+ *
+ * @author patel
+ */
+public class GitHubActionsLab {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
